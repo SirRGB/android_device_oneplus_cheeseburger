c45d4b28c2f81c351863f096ca6ac59919c9ffc4
SirRGB <sirrgb@amyrom.ml>
2022-07-09 15:00:13 +0200

    cheeseburger: bringup cherish

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_cheeseburger.mk
+    $(LOCAL_DIR)/cherish_cheeseburger.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_cheeseburger-user \
-    lineage_cheeseburger-userdebug \
-    lineage_cheeseburger-eng
+    cherish_cheeseburger-user \
+    cherish_cheeseburger-userdebug \
+    cherish_cheeseburger-eng
